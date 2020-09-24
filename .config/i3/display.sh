5567fdcc15c29157f1aa578d04f8b0d0743396fe
josepmdc <jdomingocatafal@gmail.com>
2020-09-24 16:19:17 +0200

    Refactor multiple displays script

--- .config/i3/display.sh
+++ .config/i3/display.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
-#output=$(xrandr -q | grep " connected" | cut -d ' ' -f1)
-
-# Dual monitor setup
-#if echo $output | grep -q "eDP1" && echo $output | grep -q "\bDP1\b"; then
-#  exec xrandr --output eDP1 --left-of DP1 --primary &
-#  exit 1
-#fi
-
-# Only laptop screen
-#echo $output | grep -q "eDP1" &&
-#exec xrandr --output eDP1 --mode 1920x1080 &
 
 laptop=eDP1
 thundervolt=DP1
 hdmi=HDMI1
 virtual=VIRTUAL1
 
+# Single monitor setup
 if xrandr | grep "$thundervolt disconnected"; then
-  xrandr --output "$thundervolt" --off --output "$laptop" --mode 1920x1080 --auto
-  sh ./.config/polybar/launch.sh &
+  xrandr --output "$thundervolt" --off --output "$laptop" --auto
 else
-  xrandr --output "$thundervolt" --mode 1920x1080 --output "$laptop" --primary --mode 1920x1080 --left-of "$thundervolt" 
-  sh ./.config/polybar/launch.sh &
+  xrandr --output "$thundervolt" --auto --output "$laptop" --off
 fi
+
+# Dual monitor setup
+# if xrandr | grep "$thundervolt disconnected"; then
+#   xrandr --output "$thundervolt" --off --output "$laptop" --auto
+# else
+#   xrandr --output "$thundervolt" --auto --output "$laptop" --primary \
+#   --left-of "$thundervolt" --auto
+# fi
