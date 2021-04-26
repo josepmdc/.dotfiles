e8ea8b0005d3b244395a2819273d5f20a7d71d1a
josepmdc <jdomingocatafal@gmail.com>
2021-04-26 23:37:13 +0200

    Add dependencies and remove oh my zsh from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,8 @@ echo "Installing pacman packages..."
   sudo pacman -S alacritty kitty firefox chromium neovim code dmenu rofi docker \
                  flameshot i3lock hugo nodejs go rust python vlc git fzf \
                  ranger zsh feh picom bat ripgrep playerctl exa i3 network-manager-applet \
-                 zathura zathura-pdf-mupdf texlive-most base-devel
+                 zathura zathura-pdf-mupdf texlive-most base-devel polybar fish
+                 xclip noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-fira-code
 echo "Done!"
 
 echo "Installing yay..."
@@ -20,10 +21,6 @@ echo "Done!"
 echo "Installing vim-plug..."
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-echo "Done!"
-
-echo "Installing ohmyzsh!..."
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "Done!"
 
 echo "Installing inertia zsh theme..."
