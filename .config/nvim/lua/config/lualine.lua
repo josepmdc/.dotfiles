c4f7c92b340439243a2ca9276e81da2211f81593
josepmdc <jdomingocatafal@gmail.com>
2022-07-10 18:42:37 +0200

    Change lualine color theme

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -1,44 +1,44 @@
 local colors = {
-  yellow   = '#ECBE7B',
-  green    = '#98be65',
-  red      = '#ec5f67',
-  darkblue = '#081633',
+    yellow   = '#ECBE7B',
+    green    = '#98be65',
+    red      = '#ec5f67',
+    darkblue = '#081633',
 }
 
 require('lualine').setup({
-  options = {
-    theme = 'horizon',
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = {
-      { 'branch', separator = { left = '', right = '' } },
+    options = {
+        theme = 'seoul256',
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
     },
-    lualine_b = { 
-        {
-        'diff',
-            diff_color = {
-                added = { fg = colors.green },
-                modified = { fg = colors.yellow },
-                removed = { fg = colors.red },
-            },
+    sections = {
+        lualine_a = {
+            { 'branch', separator = { left = '', right = '' } },
         },
-        'filename'},
-    lualine_c = {
-        {
-        'diagnostics',
-            sources = { 'nvim_diagnostic' },
-            symbols = { error = ' ', warn = ' ', info = ' ' },
-            diagnostics_color = {
-              color_error = { fg = colors.red },
-              color_warn = { fg = colors.yellow },
-              color_info = { fg = colors.darkblue },
+        lualine_b = {
+            {
+                'diff',
+                diff_color = {
+                    added = { fg = colors.green },
+                    modified = { fg = colors.yellow },
+                    removed = { fg = colors.red },
+                },
             },
-        }
+            'filename' },
+        lualine_c = {
+            {
+                'diagnostics',
+                sources = { 'nvim_diagnostic' },
+                symbols = { error = ' ', warn = ' ', info = ' ' },
+                diagnostics_color = {
+                    color_error = { fg = colors.red },
+                    color_warn = { fg = colors.yellow },
+                    color_info = { fg = colors.darkblue },
+                },
+            }
+        },
+        lualine_z = {
+            { 'location', separator = { right = '' }, left_padding = 2 },
+        },
     },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
-  },
 })
