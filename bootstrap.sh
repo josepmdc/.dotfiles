1a1112500b1b79637034b96e408b1de4730c0a94
josepmdc <jdomingocatafal@gmail.com>
2022-10-28 22:50:30 +0200

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 echo "Updating pacman packages and databases..."
-    sudo pacman -Syu
+sudo pacman -Syu
 echo "Done!"
 
 echo "Installing pacman packages..."
 sudo pacman -S \
-    alacritty kitty firefox chromium neovim code dmenu rofi redshift \
-    flameshot i3lock hugo nodejs go rustup python vlc git nautilus \
-    vifm feh picom bat ripgrep playerctl exa i3 network-manager-applet \
-    zathura zathura-pdf-mupdf texlive-most base-devel polybar fish \
-    xclip noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-fira-code
+    kitty firefox neovim dmenu flameshot i3lock  nodejs go rustup python vlc git \
+    nautilus vifm feh picom bat ripgrep playerctl exa i3 network-manager-applet \
+    zathura zathura-pdf-mupdf texlive-most base-devel polybar fish xclip ttf-fira-code
 echo "Done!"
 
 echo "Installing yay..."
-    git clone https://aur.archlinux.org/paru.git
-  cd yay
-  makepkg -si
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+rm -rf yay
+echo "Done!"
+
+echo "Setting keyboard layout"
+localectl set-x11-keymap es, 105, qwerty caps:swapescape
 echo "Done!"
