07ddbd24defdb300d87e81870fc804285d38dd33
josepmdc <jdomingocatafal@gmail.com>
2024-11-12 11:34:20 +0100

    random stuff

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,12 +3,9 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
+    MONITOR=$m polybar --reload main --config=.config/polybar/config.ini &
   done
 else
   polybar --reload main &
